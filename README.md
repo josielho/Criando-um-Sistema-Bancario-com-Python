5a3125a9625c0e503af841d742c3fb9319c83bfd
Josielho <josielhosena@gmail.com>
2025-07-04 14:03:05 -0300

    Otimizando o Sistema Bancário com Funções Python

--- README.md
+++ README.md
@@ -13,3 +13,19 @@ O sistema deve permitir realizar 3 saques diários com limite máximo de R$ 500,
 Essa operação deve listar todos os depósitos e saques realizados na conta. No fim da listagem deve ser exibido o saldo atual da conta. 
 Se o extrato estiver em branco, exibir a mensagem: 'Não foram realizadas movimentações.'
 Os valores devem ser exibidos utilizando o formato R$ xxx.xx, exemplo: 1500.45 = R$ 1500.45
+
+## Criar funções
+### Saque
+A função saque deve receber os argumentos apenas por nome (keyword only).
+### depósito
+A função depósito deve receber os argumentos apenas por posição (positional only).
+### Extrato
+A função extrato deve receber os argumentos por posição e nome (positional only e keyword only).
+Argumentos posicionais: saldo
+Argumentos nomeados: extrato
+
+## Novas funções
+### Criar usuário (cliente)
+O programa deve armazenar os números em uma lista, um usuário é composto por: nome, data de nascimento, cpf e endereço. O endereço é uma string com o formato: logradouro, nro - bairro - cidade/sigla estado. Deve ser armazenado somente os números do CPF. Não podemos cadastrar 2 usuários com o mesmo CPF.
+### Criar conta corrente
+O programa deve armazenar contas em uma lista, uma conta é composta por: agência, número da conta e usuário. O número da conta é sequencial, iniciando em 1. O número da agência é fixo: '0001'. O usuário pode ter mais de uma conta, mas uma conta pertence a somente um usuário.
--- Solução/solucao.py
+++ Solução/solucao.py
@@ -1,5 +1,7 @@
 menu = '''
 
+[u] Criar usuário
+[c] Criar conta corrente
 [d] Depositar
 [s] Sacar
 [e] Extrato
@@ -7,20 +9,121 @@ menu = '''
 
 => '''
 
-mensagem_depositar = f'''
+mensagem_depositar = '''
 Você selecionou a opção Depositar!
 Informe o valor do depósito:
 => '''
-mensagem_sacar = f'''
+mensagem_sacar = '''
 Você selecionou a opção Sacar!
 Informe o valor do saque:
+=> '''
+mensagens_criar_usuario = [
+{'chave' : 'nome',
+'mensagem' :'''
+Você selecionou a opção Criar usuário!
+Informe o nome do usuário:
+=> '''},
+{'chave' : 'data_de_nascimento',
+'mensagem' :'''
+Informe a data de nascimento do usuário, ex: 01/01/2001
+=> '''},
+{'chave' : 'cpf',
+'mensagem' :'''
+Informe o cpf do usuário, ex: 111.222.333-44
+=> '''},
+{'chave' : 'endereco',
+'mensagem' :'''
+Informe o endereço do usuário, ex: logradouro, nro - bairro - cidade/sigla estado
+=> '''}]
+
+mensagem_criar_conta = '''
+Você selecionou a opção Criar conta corrente!
+Informe o cpf do usuário cadastrado, ex: 111.222.333-44
 => '''
 
 saldo = 0
 limite = 500
 extrato = ''
 numero_saques = 0
+lista_users = []
+lista_contas = []
 LIMITE_SAQUES = 3
+ultimo_numero_da_conta = 0
+AGENCIA = '0001'
+
+#POSITIONAL ONLY
+def Deposito( saldo, valor_deposito, extrato, /):
+    saldo += valor_deposito
+    mensagem_deposito_sucesso = '\n=====================DEPÓSITO=====================\n'
+    mensagem_deposito_sucesso += f'Depósito de R$: {valor_deposito:.2f}\n'
+    mensagem_deposito_sucesso += '=================================================='
+    extrato += mensagem_deposito_sucesso
+    print(mensagem_deposito_sucesso)
+    return saldo, extrato
+
+#KEYWORD ONLY
+def Saque( *, saldo, valor_saque, numero_saques, extrato):
+    saldo -= valor_saque
+    numero_saques += 1
+    mensagem_saque_sucesso = '\n======================SAQUE=======================\n'
+    mensagem_saque_sucesso += f'Saque de R$: {valor_saque:.2f}\n'
+    mensagem_saque_sucesso += '=================================================='
+    extrato += mensagem_saque_sucesso
+    print(mensagem_saque_sucesso)
+    return numero_saques, saldo, extrato
+
+#saldo -> POSITIONAL ONLY && extrato -> KEYWORD ONLY
+def Extrato( saldo, / , *, extrato):
+    if extrato:
+        print(extrato)
+        print(f'Saldo atual R$ {saldo:.2f}')
+    else:
+        print('Não foram realizadas movimentações.')
+        print(f'Saldo atual R$ {saldo:.2f}')
+
+def Validar_cpf( valorcpf, lista_users):
+    str_cpf = ''
+    cpf_existe = False
+    for caracter in list(valorcpf):
+        try:
+            int(caracter)
+        except:
+            pass
+        else:
+            str_cpf += caracter
+    for dict_user in lista_users:
+        if dict_user['cpf'] == str_cpf:
+            cpf_existe = True
+            break
+    return str_cpf, cpf_existe
+    
+def Add_conta_new_cpf( agencia, ultimo_numero_da_conta, str_cpf,lista_contas):
+    dict_new_conta = dict(agencia = agencia, numero_da_conta = [ultimo_numero_da_conta])
+    dict_cpf = {str_cpf : dict_new_conta}
+    lista_contas.append(dict_cpf)
+
+
+def Add_conta( str_cpf, cpf_existe, ultimo_numero_da_conta, lista_contas, agencia):
+    if cpf_existe:
+        ultimo_numero_da_conta += 1
+
+        if lista_contas:
+            conta_cpf_existe = False
+            for contas_cpf in lista_contas:
+                if str_cpf in contas_cpf:
+                    conta_cpf_existe = True
+                    contas_cpf[str_cpf]['numero_da_conta'].append(ultimo_numero_da_conta)
+                    break
+        if (not lista_contas) or (not conta_cpf_existe):
+            Add_conta_new_cpf( agencia, ultimo_numero_da_conta, str_cpf, lista_contas)
+
+        print(lista_contas)
+
+    else:
+        print('Nenhum usuário registrado na base para o cpf informado, por favor registre um usuário com esse cpf.')
+
+    return ultimo_numero_da_conta
+
 
 while True:
     opcao = input(menu)
@@ -34,12 +137,7 @@ while True:
             if valor_deposito <= 0 :
                 print('Valores menores ou iguais a zero não podem ser utilizados, por favor selecione novamente a operação desejada.')
             else:
-                saldo += valor_deposito
-                mensagem_deposito_sucesso = '\n=====================DEPÓSITO=====================\n'
-                mensagem_deposito_sucesso += f'Depósito de R$: {valor_deposito:.2f}\n'
-                mensagem_deposito_sucesso += '=================================================='
-                extrato += mensagem_deposito_sucesso
-                print(mensagem_deposito_sucesso)
+                saldo, extrato = Deposito( saldo, valor_deposito, extrato)
             
     elif opcao == 's':
         if numero_saques >= LIMITE_SAQUES:
@@ -58,21 +156,34 @@ while True:
                     elif valor_saque > limite:
                         print('O valor informado é superior aos R$ 500 permitidos por saque, por favor selecione novamente a operação desejada.')
                     else:
-                        saldo -= valor_saque
-                        numero_saques += 1
-                        mensagem_saque_sucesso = '\n======================SAQUE=======================\n'
-                        mensagem_saque_sucesso += f'Saque de R$: {valor_saque:.2f}\n'
-                        mensagem_saque_sucesso += '=================================================='
-                        extrato += mensagem_saque_sucesso
-                        print(mensagem_saque_sucesso)
+                        numero_saques, saldo, extrato = Saque( saldo = saldo, valor_saque = valor_saque, numero_saques = numero_saques, extrato = extrato)
     
     elif opcao == 'e':
-        if extrato:
-            print(extrato)
-            print(f'Saldo atual R$ {saldo:.2f}')
+        Extrato(saldo, extrato = extrato)
+
+    elif opcao == 'u':
+        new_dict_user = {}
+        for dicionario in mensagens_criar_usuario:
+            if dicionario['chave'] == 'cpf':
+                valorcpf = input(dicionario['mensagem'])
+                str_cpf, cpf_existe = Validar_cpf(valorcpf, lista_users)
+
+                if cpf_existe:
+                    break
+                new_dict_user['cpf'] = str_cpf
+            else:
+                new_dict_user[dicionario['chave']] = input(dicionario['mensagem'])
+
+        if not cpf_existe:
+            lista_users.append(new_dict_user)
+            print(new_dict_user)
         else:
-            print('Não foram realizadas movimentações.')
-            print(f'Saldo atual R$ {saldo:.2f}')
+            print('Já existe um usuário registrado na base para o cpf informado!')
+
+    elif opcao == 'c':
+        valorcpf = input(mensagem_criar_conta)
+        str_cpf, cpf_existe = Validar_cpf( valorcpf, lista_users = lista_users)
+        ultimo_numero_da_conta = Add_conta( str_cpf, cpf_existe, ultimo_numero_da_conta, lista_contas, AGENCIA)
 
     elif opcao == 'q':
         break
